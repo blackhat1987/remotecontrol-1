25a856ef185645bebb633b6b947c5e6a2c7e4f4c
guanginuestc <h_n_sjg@163.com>
2018-06-02 22:43:24 +0800

    增加文件传输功能

--- MySocket/MySocket.cpp
+++ MySocket/MySocket.cpp
@@ -11,26 +11,30 @@ MySocket::~MySocket()
 	closesocket(sockClient);
 	WSACleanup();
 }
-bool MySocket::SocketInit(const string ipaddr,unsigned short port) {
+bool MySocket::SocketInit(const string ipaddr,unsigned short port, bool flag) {
 	WORD wVersionRequested;
 	WSADATA wsaData;
 	wVersionRequested = MAKEWORD(2, 2);
 	if (WSAStartup(wVersionRequested, &wsaData)) {
 		return false;
 	}
-	/*if (LOBYTE(wsaData.wVersion) != 1 || HIBYTE(wsaData.wVersion) != 1) {
-		WSACleanup();
-		return false;
-	}*/
 	sockClient = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	addrClient.sin_addr.S_un.S_addr = inet_addr(ipaddr.c_str());
 	addrClient.sin_family = AF_INET;
 	addrClient.sin_port = htons(port);
-	if (connect(sockClient, (sockaddr *)&addrClient, sizeof(addrClient)) == SOCKET_ERROR)
-	{  //连接失败   
-		printf("connect error !");
-		closesocket(sockClient);
-		return 0;
+	if (flag) {
+		if (bind(sockClient, (LPSOCKADDR)&addrClient, sizeof(addrClient) == SOCKET_ERROR)) {
+			return false;
+		}
+		/*服务器*/
+	}
+	else {
+		if (connect(sockClient, (sockaddr *)&addrClient, sizeof(addrClient)) == SOCKET_ERROR)
+		{  //连接失败   
+			printf("connect error !");
+			closesocket(sockClient);
+			return 0;
+		}
 	}
 	return TRUE;
 }
@@ -42,5 +46,30 @@ bool MySocket::SocketRecv(string & recvbuf) {
 	CHAR BUF[100];
 	int rec=recv(sockClient, BUF, 100, 0);
 	recvbuf = BUF;
+	return true;
+}
+bool MySocket::SocketSendF(string &filename) {
+	ifstream f1;
+	f1.open(filename,ios::binary|ios::in);
+	struct SIZE {
+		int len;
+		BYTE end;
+	};
+	SIZE s = {0,0};
+	f1.seekg(0, ios::end);
+	int axx=0;
+	s.len= f1.tellg();
+	s.len++;
+	s.end = '\0';
+	cout << s.len << endl;
+	cout << s.end << endl;
+	f1.seekg(0, ios::beg);
+	PCHAR buffer = new CHAR[s.len];
+	f1.read(buffer, s.len);
+	f1.close();
+	
+	send(sockClient, (CHAR*)&s, sizeof SIZE, 0);
+	send(sockClient, buffer, s.len, 0);
+	delete[]buffer;
 	return true;
 }
